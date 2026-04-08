446ea91622943256cd4712edd0a2c2ef0045593a
masha <mary200438@gmail.com>
2026-04-09 02:41:58 +0300

    feature(PWPT-30): Главный экран

--- src/components/ui/shared/app-sidebar.tsx
+++ src/components/ui/shared/app-sidebar.tsx
@@ -2,13 +2,14 @@ import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
+  SidebarFooter,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar.tsx"
-import { BarChart, FileText, Shield, Users } from "lucide-react"
+import { BarChart, FileText, Shield, User, Users } from "lucide-react"
 
 export function AppSidebar() {
   return (
@@ -38,17 +39,26 @@ export function AppSidebar() {
                   <span className="ml-2">Тесты</span>
                 </SidebarMenuButton>
               </SidebarMenuItem>
-
               <SidebarMenuItem>
                 <SidebarMenuButton>
                   <BarChart className="h-5 w-5" />
                   <span className="ml-2">Статистика</span>
                 </SidebarMenuButton>
               </SidebarMenuItem>
-
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+
+        <SidebarFooter className="mt-auto p-2">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <User className="w-6 h-6 rounded-full" />
+                <span className="ml-2">Maria</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
       </SidebarContent>
     </Sidebar>
   )
