c1f7b5edf06718f31f4b5b11f6aafe53d16e879d
masha <mary200438@gmail.com>
2026-04-09 03:08:44 +0300

    feature(PWPT-30): Главный экран

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,15 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/ui/shared/app-sidebar.tsx"
 import * as React from "react"
+import { AppSidebar } from "@/components/ui/shared/app-sidebar.tsx"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-    <main>
-      <SidebarTrigger />
-    {children}
-    </main>
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
     </SidebarProvider>
   )
 }
