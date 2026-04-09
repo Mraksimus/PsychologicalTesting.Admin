cc6c131a10493ee1ab89be3101c75b3201076d45
masha <mary200438@gmail.com>
2026-04-09 23:14:47 +0300

    feature(PWPT-30): Главный экран

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,30 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import * as React from "react"
-import { AppSidebar } from "@/components/ui/shared/app-sidebar.tsx"
+import { AppSidebar } from "@/components/ui/shared/app-sidebar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <div className="flex h-screen w-screen overflow-hidden">
+
+        {/* Sidebar */}
+        <AppSidebar />
+
+        {/* Main content */}
+        <main className="flex flex-1 flex-col overflow-auto">
+
+          {/* Top bar */}
+          <div className="p-2 border-b">
+            <SidebarTrigger />
+          </div>
+
+          {/* Page content */}
+          <div className="flex-1 p-6">
+            {children}
+          </div>
+
+        </main>
+      </div>
     </SidebarProvider>
   )
 }
