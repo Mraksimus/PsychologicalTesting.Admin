f57af4ccbf30dc0dfa4648e79f5668da95e503a4
masha <mary200438@gmail.com>
2026-04-20 21:51:12 +0300

    feature(PWPT-34): Страница тестов

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "@/pages/Login.tsx"
 import Home from "@/pages/home/Home.tsx"
-
+import TestCreate from "@/pages/createTests/TestCreate"
 
 export default function App() {
   return (
@@ -9,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/tests/create" element={<TestCreate />} />
       </Routes>
     </BrowserRouter>
   )
